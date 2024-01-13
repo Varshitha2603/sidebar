03663bcdf85ff35daeca0af39ced520f9216079b
DCKAP <varshithaksdckap@gmail.com>
2024-01-13 14:26:39 +0530

    sidebar

---
+++ main_side_bar.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let con = document.getElementById("container");
+
+let cross_icon = document.getElementById("icon2");
+
+let bar_icon = document.getElementById("icon1");
+
+bar_icon.addEventListener("click",showBaricon)
+
+function showBaricon()
+{
+    side_bar.classList.add(`active`);
+}
+
+cross_icon.addEventListener("click",hideBaricon)
+function hideBaricon()
+{
+    side_bar.classList.remove(`active`);
+}
+
